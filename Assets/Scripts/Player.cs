0fb80ed3f486571588188d43bc663615bd35404a
elzgame <elzgamedev@gmail.com>
2021-09-10 09:01:00 +0700

    Done, probably

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
@@ -8,22 +8,59 @@ using Unity.MLAgents.Sensors;
 public class Player : Agent
 {
 
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer groundMeshRenderer;
 
-
-    public override void CollectObservations(VectorSensor sensor) {
-        sensor.AddObservation(transform.position);
-        sensor.AddObservation(target.transform.position);
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(1.4f, 4f), transform.localPosition.y, Random.Range(-1, 5.5f));
+        targetTransform.localPosition = new Vector3(Random.Range(-1f, -5f), targetTransform.localPosition.y, Random.Range(-1.5f, 5.8f));
     }
 
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.transform.localPosition);
+    }
 
-    public override void OnActionReceived(ActionBuffers actions) {
+    public override void OnActionReceived(ActionBuffers actions)
+    {
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
 
-    
+        float moveSpeed = 20f;
+
+        transform.localPosition += new Vector3(moveX, 0f, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousSegment = actionsOut.ContinuousActions;
         float moveSpeed = 1f;
-        transform.position = new Vector3(moveX, 0f, moveZ) * Time.deltaTime * moveSpeed;
+
+        continuousSegment[0] = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        continuousSegment[1] = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        Debug.Log("MOVEX : " + continuousSegment[0]);
+        Debug.Log("MOVEZ : " + continuousSegment[1]);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            SetReward(+1f);
+            groundMeshRenderer.material = winMaterial;
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            groundMeshRenderer.material = loseMaterial;
+            EndEpisode();
+        }
     }
 
 }
